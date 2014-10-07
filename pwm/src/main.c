6bb7b3c2886fe4798be1008653332535f0dcaf74
gokhanBeken <gokhan60@gmail.com>
2014-10-07 03:26:56 +0300

    timer4 eklendi
    sadece timer 3 vardı, ayrıca timer 4 ekledim, d12-15 arasındaki ledleri
    de pwm ile kontrol ediyoruz

--- pwm/src/main.c
+++ pwm/src/main.c
@@ -12,21 +12,29 @@ uint16_t Prescaler = 84;
 uint32_t Period = 20000;
 
 /* Pulse Values for Channel 1,2,3,4 */
-uint32_t PulseValue1 = 20;     // Channel1 Duty Cycle %0  = (0/20000)*100
+uint32_t PulseValue1 = 0;     // Channel1 Duty Cycle %0  = (0/20000)*100
 uint32_t PulseValue2 = 50000; // Channel2 Duty Cycle %25 = (5000/20000)*100
 uint32_t PulseValue3 = 10000; // Channel1 Duty Cycle %50 = (10000/20000)*100
 uint32_t PulseValue4 = 15000; // Channel1 Duty Cycle %75 = (15000/20000)*100
 
-void Pin_Config(void);
+void Pin3_Config(void);
 void Timer3_PWM_Output_Config(uint32_t PeriodValue, uint16_t PrescalerValue);
+
+void Pin4_Config(void);
+void Timer4_PWM_Output_Config(uint32_t PeriodValue, uint16_t PrescalerValue);
+
 void DelayMs(unsigned int ms);
 
 int main(void)
 {
     SystemInit();
     SysTick_Config(SystemCoreClock / 1000); //1milisaniyelik system clock elde edelim
-    Pin_Config();
+
+    Pin3_Config();
     Timer3_PWM_Output_Config(Period, Prescaler);
+
+    Pin4_Config();
+    Timer4_PWM_Output_Config(Period, Prescaler);
 
     unsigned int sayac1=0,sayac2=0,sayac3=0,sayac4=0;
     char durum1=1,durum2=1,durum3=1,durum4=1; //1=artma, 0=eksilme
@@ -82,15 +90,21 @@ int main(void)
         }
 
 
+        TIM_SetCompare1(TIM4,sayac1);
+        TIM_SetCompare2(TIM4,sayac2);
+        TIM_SetCompare3(TIM4,sayac3);
+        TIM_SetCompare4(TIM4,sayac4);
+
         TIM_SetCompare1(TIM3,sayac1);
         TIM_SetCompare2(TIM3,sayac2);
         TIM_SetCompare3(TIM3,sayac3);
         TIM_SetCompare4(TIM3,sayac4);
+
         DelayMs(1);
     }
 }
 
-void Pin_Config(void)
+void Pin3_Config(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
@@ -167,6 +181,90 @@ void Timer3_PWM_Output_Config(uint32_t PeriodValue, uint16_t PrescalerValue)
 
     /* TIM3 enable counter */
     TIM_Cmd(TIM3, ENABLE);
+
+}
+
+
+
+
+void Pin4_Config(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    /* Enable clock for TIM4 */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+    /* GPIOD and GPIOB clock enable */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOB, ENABLE);
+
+    /* GPIOC Configuration: TIM4 CH1 (PD12) and TIM4 CH2 (PD13) TIM3 CH4 (PD14) and TIM4 CH4 (PD15)*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15 ;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+
+    /* Connect TIM4 pins to AF2 */
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
+
+    /******************************************************************/
+}
+
+
+void Timer4_PWM_Output_Config(uint32_t PeriodValue, uint16_t PrescalerValue)
+{
+
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+    TIM_OCInitTypeDef  TIM_OCInitStructure;
+
+    /* Time base configuration */
+    TIM_TimeBaseStructure.TIM_Period = PeriodValue - 1;
+    TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue - 1;
+    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+
+    /* PWM1 Mode configuration */
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+    /* PWM1 Mode configuration: Channel1 */
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = PulseValue1;
+
+    TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    /* PWM1 Mode configuration: Channel2 */
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = PulseValue2;
+
+    TIM_OC2Init(TIM4, &TIM_OCInitStructure);
+    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    /* PWM1 Mode configuration: Channel3 */
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = PulseValue3;
+
+    TIM_OC3Init(TIM4, &TIM_OCInitStructure);
+    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    /* PWM1 Mode configuration: Channel4 */
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = PulseValue4;
+
+    TIM_OC4Init(TIM4, &TIM_OCInitStructure);
+    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_ARRPreloadConfig(TIM4, ENABLE);
+
+    /* TIM4 enable counter */
+    TIM_Cmd(TIM4, ENABLE);
 
 }
 
